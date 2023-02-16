f4512d0dd0158c8a0469913c0c0d2e5796ed5f34
Charles COLELLA <charlescol@hotmail.fr>
2023-02-16 00:12:08 -0500

    [CODE CLEANING]

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 export * from "./src/matches";
 export * from './src/profile'
 export * from './src/messages'
-export  * from './src/common/type'
-export  * from './src/common/enum'
+export * from './src/common/type'
+export * from './src/common/enum'
